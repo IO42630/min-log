c6e28aa2cd2da45963d3b6571d36b5e78b74755a
io42630 <ivan@olexyn.com>
2022-08-11 11:43:39 +0200

    + skip github pkg, just use local jars

---
+++ install-locally.ps1
@@ -0,0 +1,8 @@
+$version="0.1"
+$packaging="jar"
+$file="target\min-log-$version.$packaging"
+$groupId="com.olexyn.min.log"
+$artifactId="min-log"
+
+mvn package
+mvn install:install-file -Dfile="$file" -DgroupId="$groupId" -DartifactId="$artifactId" -Dversion="$version" -Dpackaging="$packaging" -DgeneratePom="true"
